757d3825a2660f7fa59062067e1fe3a443b3c856
Blake C <bcortri1@kent.edu>
2022-08-25 14:17:17 -0400

    Complete server tests

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,12 +11,29 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+    serverNameInput.value = 'Frank';
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(2);
+    expect(allServers['server' + serverId].serverName).toEqual('Frank');
+  });
+
+  it('Should update the server table with empty request', function () {
+    updateServerTable();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+    expect(serverTbody.innerHTML).toEqual('');
+  });
+
 
   afterEach(function() {
     // teardown logic
     serverNameInput.value = '';
     allServers = {};
     serverId = 0;
-    document.querySelector("#server1").remove();
+    if (document.querySelector("#server1")) {
+      document.querySelector("#server1").remove()
+    };
   });
 });
