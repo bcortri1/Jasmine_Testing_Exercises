cf2fd47e20f4d2cf2640e48df15caf9eb042b423
Blake C <bcortri1@kent.edu>
2022-08-24 23:33:16 -0400

    minor changes

--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -1,11 +1,27 @@
 beforeEach(function () {
-    
+
 });
 
 describe("Testing createCurPayment()", function() {
-    it('', function () {
 
+    it('Should return undefined', function () {
+        billAmtInput.value = -1;
+        tipAmtInput.value = -1;
+        expect(createCurPayment()).toEqual(undefined);
     });
+
+    it('Should return correct bill amount', function () {
+        billAmtInput.value = 100;
+        tipAmtInput.value = 0;
+        expect(createCurPayment()).toEqual({billAmt: 100, tipAmt: 0, tipPercent: 0});
+    });
+
+    it('Should return undefined', function () {
+        billAmtInput.value = '';
+        tipAmtInput.value = '';
+        expect(createCurPayment()).toEqual(undefined);
+    });
+
 });
 
 describe("Testing appendPaymentTable()", function() {
@@ -16,6 +32,8 @@ describe("Testing appendPaymentTable()", function() {
 
 describe("Testing updateSummary()", function() {
     it('', function () {
+        paymentTotal = sumPaymentTotal('tipPercent');
+        
     });
 });
 
@@ -26,5 +44,7 @@ describe("Testing submitPaymentInfo()", function() {
 });
 
 afterEach(function() {
+    billAmtInput.value = '';
+    tipAmtInput.value = '';
 
 });
