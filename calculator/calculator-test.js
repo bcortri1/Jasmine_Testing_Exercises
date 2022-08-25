1366bd88cb7aa071afef48c5da7d47addc20da10
Blake C <bcortri1@kent.edu>
2022-08-24 20:46:29 -0400

    Added Decimal test to Jasmine

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,6 +1,6 @@
 
 it('should calculate the monthly payment correctly', function () {
-  // ...
+  
   let values = {amount: 0, rate: 1,years: 1};
   expect(calculateMonthlyPayment(values)).toEqual('0.00');
 
@@ -10,13 +10,15 @@ it('should calculate the monthly payment correctly', function () {
   values = {amount: 1000, rate: 0,years: 10};
   expect(calculateMonthlyPayment(values)).toEqual('8.33');
 
+  values = {amount: 10000, rate: 4.5,years: 1};
+  expect(calculateMonthlyPayment(values)).toEqual('3833.95');
+
 
 });
 
 
 it("should return a result with 2 decimal places", function() {
 //redundant
-
   values = {amount: 10000, rate: 10,years: 15};
   expect(calculateMonthlyPayment(values)).toEqual('8333.33');
 });
