a67677b3c68e06462b757e270f3f24b0804a6f63
Blake C <bcortri1@kent.edu>
2022-08-24 21:54:38 -0400

    testing framework

---
+++ tip-pool/payments.test.js
@@ -0,0 +1,30 @@
+beforeEach(function () {
+    
+});
+
+describe("Testing createCurPayment()", function() {
+    it('', function () {
+
+    });
+});
+
+describe("Testing appendPaymentTable()", function() {
+    it('', function () {
+
+    });
+});
+
+describe("Testing updateSummary()", function() {
+    it('', function () {
+    });
+});
+
+describe("Testing submitPaymentInfo()", function() {
+    it('', function () {
+
+    });
+});
+
+afterEach(function() {
+
+});
